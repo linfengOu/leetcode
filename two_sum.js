76c29b6c128490a99a1e2f16b067b0268e724a56
Oliver <linfeng.ou@gmail.com>
2019-07-18 13:50:47 +0800

    Create two_sum.js

---
+++ two_sum.js
@@ -0,0 +1,43 @@
+/**
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+*/
+
+/** Version 1
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j];
+            }
+        }
+    }
+};
+
+/** Version 2
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+*/
+var twoSum = function(nums, target) {
+    let map = {}, complement;
+    for (let i = 0; i < nums.length; i++) {
+        map[nums[i]] = i;
+    }
+    for (let i = 0; i < nums.length; i++) {
+        complement = target - nums[i];
+        if (map.hasOwnProperty(complement) && map[complement] !== i) {
+            return [i, map[complement]];
+        }
+    }
+};
